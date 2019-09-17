8ea6b65029c1b89bf4f71069961b3b7e5c11e84b
Damon <282823740@qq.com>
2019-09-17 16:42:48 +0800

    字符串

---
+++ Day7/字符串.py
@@ -0,0 +1,11 @@
+def main():
+    str1 = 'hello World'
+    print(len(str1))
+    print(str1.capitalize())
+    print(str1.upper())
+    print(str1[2:8])
+    print(str1.upper().casefold())
+
+
+if __name__ == '__main__':
+    main()

da144fdb9eb8be23da750f3a3de8712e3608b41d
Damon <282823740@qq.com>
2019-09-17 11:45:57 +0800

    模块和作用域

---
+++ Day6/function1.py
@@ -0,0 +1,33 @@
+from random import randint
+
+
+def roll_dice(n=2):
+    """
+    摇色子
+
+    :param n: 色子的个数
+    :return: n颗色子点数之和
+    """
+    total = 0
+    for _ in range(n):
+        total += randint(1, 6)
+    return total
+
+
+def add(*args):
+    total = 0
+    for val in args:
+        total += val
+    return total
+
+
+# 如果没有指定参数那么使用默认值摇两颗色子
+print(roll_dice())
+# 摇三颗色子
+print(roll_dice(3))
+print(add())
+print(add(1))
+print(add(1, 2))
+print(add(1, 2, 3))
+# 传递参数时可以不按照设定的顺序进行传递
+print(add(50, 100, 200))
---
+++ Day6/function2.py
@@ -0,0 +1,7 @@
+import Day6.mudules1 as m1
+import Day6.modules2 as m2
+import Day6.modules3 as m3
+
+m1.foo()
+m2.foo()
+m3.foo()

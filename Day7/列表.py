817910c99167c6042295457eb3174eacfd2c0ad2
Damon <282823740@qq.com>
2019-09-19 14:41:17 +0800

    生成器

--- Day7/列表.py
+++ Day7/列表.py
@@ -1,3 +1,13 @@
+import sys
+
+
+def fib(n):
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+        yield a
+
+
 def main():
     list1 = [1, 2, 3, 4, 5]
     print(list1)
@@ -14,6 +24,15 @@ def main():
     print(list1)
     del list2[1]
     print(list2)
+    f = [x for x in range(1, 10)]
+    print(f)
+    f = [x ** 2 for x in range(1, 1000)]
+    print(f)
+    print(sys.getsizeof(f))
+    for val in f:
+        print(val)
+    for val in fib(20):
+        print(val, end=' ')
 
 
 if __name__ == "__main__":
